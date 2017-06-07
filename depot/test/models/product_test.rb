85b6941de2881f7585fe282aa6563059ed2104be
KellyLaas <laas@iit.edu>
2017-06-07 14:33:30 -0500

    Define banner and sidebar in layouts/application.html.erb

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures : products
+  # ...
+end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -27,4 +31,33 @@ test "product price must be positive" do
   product.price = 1
   assert product.valid?
 end
+
+def new_product(image_url)
+  Product.new(title: "My Book Title",
+              description: "yyy",
+              price: 1,
+              image_url: image_url)
+end
+
+test "image url" do
+  ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+            http://a.b.c/x/y/z/fred.gif }
+  bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+  ok.each do |name|
+    assert new_product(name).valid?, "#{name} shouldn't be invalid"
+  end
+
+  bad.each do |name|
+    assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  end
+end
+
+test "product is not valid without a unique title" do
+  product = Product.new(title: products(:ruby).title,
+                        description: "yyy",
+                        price: 1,
+                        image_url: "fred.gif")
+  assert product.invalid?
+  assert_equal ["has already been taken"], product.errors[:title] end
 end
