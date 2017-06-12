5f1c6998be887d09fb5881cd5bc045f8759105c6
KellyLaas <laas@iit.edu>
2017-06-12 16:05:52 -0500

    Add seed data

--- ethicscases/db/schema.rb
+++ ethicscases/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612201702) do
+ActiveRecord::Schema.define(version: 20170612202115) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170612201702) do
     t.index ["case_id", "author_id"], name: "index_authors_cases_on_case_id_and_author_id"
   end
 
+  create_table "authors_subjects", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "subject_id", null: false
+    t.index ["author_id", "subject_id"], name: "index_authors_subjects_on_author_id_and_subject_id"
+    t.index ["subject_id", "author_id"], name: "index_authors_subjects_on_subject_id_and_author_id"
+  end
+
   create_table "cases", force: :cascade do |t|
     t.string "title"
     t.string "year"
@@ -34,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170612201702) do
     t.string "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cases_subjects", id: false, force: :cascade do |t|
+    t.integer "case_id", null: false
+    t.integer "subject_id", null: false
+    t.index ["case_id", "subject_id"], name: "index_cases_subjects_on_case_id_and_subject_id"
+    t.index ["subject_id", "case_id"], name: "index_cases_subjects_on_subject_id_and_case_id"
   end
 
   create_table "subjects", force: :cascade do |t|
--- ethicscases/db/seeds.rb
+++ ethicscases/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Case.destroy_all
+
+Case:create! ([{
+  title: "The Whistle-Blower",
+  author: "Peggy Connolley",
+  abstract: "Research assistant blows whistle on supervisor.",
+  year: "1999",
+  subject: "whistleblowing",
+  subject: "mentoring",
+},
+
+{
+  title: "Immigration woe",
+  author: "Kelly Laas",
+  abstract: "Student stuck in bad TA position due to immigration status,
+              overworked and underpaid",
+  year: "2016",
+  subject: "mentoring",
+  subject: "culture"
+}])
+
+p "Created #{Case.count} cases"
