304c155970b24e67ed6bbe556fd9cfabee0ff3ef
KellyLaas <laas@iit.edu>
2017-06-12 16:34:36 -0500

    Allow for full name to be entered in model/author

--- ethicscases/app/models/author.rb
+++ ethicscases/app/models/author.rb
@@ -2,6 +2,16 @@ class Author < ApplicationRecord
   has_and_belongs_to_many :cases
   has_and_belongs_to_many :subjects
 
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def name_from_parts(name)
+    name_parts = name.split(" ")
+    self.last_name = name_parts.pop
+    first_name = name_parts.join(" ")
+  end
+
   validates :first_name, :last_name, presence: true
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :first_name, :last_name, uniqueness: true
--- ethicscases/test/models/case_test.rb
+++ ethicscases/test/models/case_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def new_case
+    Case.new(title: "My Case Title",
+              year: "yyyy"
+              author: "yyy"
+              abstract: "yyyy"
+              subject: "yyy")
+end
+
+  test "should have the necessary required validators" do
+    case = Case.new
+    assert_not case.valid?
+    assert_equal [:title, :year, :author, :abstract, :subject], case.error.keys 
+
 end
