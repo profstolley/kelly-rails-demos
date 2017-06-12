b80f73fbb58de1e99ef90977e28d8ecbb28c9d08
KellyLaas <laas@iit.edu>
2017-06-12 14:28:22 -0500

    Run migration for author

--- ethicscases/db/schema.rb
+++ ethicscases/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612192353) do
+ActiveRecord::Schema.define(version: 20170612192743) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cases", force: :cascade do |t|
     t.string "title"
